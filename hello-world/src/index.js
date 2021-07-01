fc4ff19f6c585582b67deaaccfededb53254daba
Saurav32109 <sauravgupta8521@gmail.com>
2021-07-01 16:11:56 +0530

    changes for rendering elements

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -29,29 +29,91 @@ import reportWebVitals from './reportWebVitals';
 
 
 //jsx in an expression too
-function formatuser(user)
-{
-  return user.firstName +' '+user.lastName;
-}
-const user={
-  firstName:"saurav",
-  lastName:"gupta"
-};
-function getGreeting(user){
-  if(user){
-    return <h1>Hello,{formatuser(user)}!</h1>
-  }
-  return <h1>Hello, stranger.</h1>
-}
-const element= (
-  <h1>Hello, {getGreeting(user)}! </h1>
-);
-ReactDOM.render()
-(
-  element,
-  document.getElementById('root')
-);
+//getting error in this
+// function formatuser(user)
+// {
+//   return user.firstName +' '+user.lastName;
+// }
+// const user={
+//   firstName:"saurav",
+//   lastName:"gupta"
+// };
+// function getGreeting(user){
+//   if(user){
+//     return <h1>Hello,{formatuser(user)}!</h1>
+//   }
+//   return <h1>Hello, stranger.</h1>
+// }
+// const element= (
+//   <h1>Hello, {getGreeting(user)}! </h1>
+// );
+// ReactDOM.render()
+// (
+//   element,
+//   document.getElementById('root')
+// );
+
+
+
+
+
+
+//3.//Rendering elements
+
+//<div> id="root"</div> this is a root DOM node
+//to render a react  element into root DOM node we pass both to ReactDOM.render():
+//const element=<h1>Hello, saurav how have you been</h1>;
+//ReactDOM.render(element,document.getElementById('root'));
+
+
+//updating the rendered elements:since reacts elements are immutable it's not possible for us ot change the attributes of it's children,the only way to update ui is to create a new element, and pass it to ReactDOM.render().
+//example:ticking clock
+// function tick()
+// {
+//   const element=(
+//     <div>
+//       <h1>Hey saurav!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(element,document.getElementById('root'));
+// }
+// setInterval(tick,2000);
+//It calls ReactDOM.render() every two second from setInterval() callback.
+
+
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+
+
+
+
+
+//4//components and props
+//function and class components
+// function welcome(props)//props is an arbitrary input
+// {
+//   return <h1>Hello, {props.name}</h1>
+// }
+//or 
+// class welcome extends React.Component{
+//   render(){
+//     return <h1>Hello, {this.props.name}</h1>
+//   }
+// }
+
+//4(a)//Rendering a component
+//const element=<div />;
+function w(props){
+  return <h1>Hello, {props.name}</h1>;
+}
+const e=<w name="saurav" />;
+ReactDOM.render(
+  e,document.getElementById('root')
+);
 reportWebVitals();
